b77a28283cfd39bc8c1b6f6ebce08ff8ce554a92
Victor <victor@seald.io>
2019-11-14 18:13:52 +0100

    first commit / base installed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.scss'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Minesweecheerz</h1>
+    </header>
+  )
+}
+
+export default Header
