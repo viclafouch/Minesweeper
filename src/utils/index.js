ec7664356b2d8b55198c8782fecda80d35799fe0
viclafouch <victor.dlf@outlook.fr>
2019-11-16 20:08:30 +0100

    remove logs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 export const randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
-export const debug = str => console.log(`%c ${str}`, 'color: yellow; font-weight: bold')
+export const debug = str => process.env.NODE_ENV !== 'production' && console.log(`%c ${str}`, 'color: yellow; font-weight: bold')
